7580e8266804fd8d6ebe1a34659df0bb5113c12e
Farhad Taebi <f.taebi@faber.de>
2016-07-21 06:39:11 -0700

    Add accessor fo csv file imports

--- lib/ecto_repo.ex
+++ lib/ecto_repo.ex
@@ -11,15 +11,31 @@ defmodule ExUtils.Ecto.Repo do
       Count the number of records in the model table
       """
       def count(model) do
-        query = "SELECT COUNT(*) FROM #{model.__schema__(:source)}"
+        query = "SELECT COUNT(*) FROM #{table(model)}"
         {:ok, %{rows: [[n_rows]]}} = Ecto.Adapters.SQL.query(__MODULE__, query, [])
         n_rows
       end
 
       def last_id(model) do
-        query = "SELECT last_value AS id FROM #{model.__schema__(:source)}_id_seq"
+        query = "SELECT last_value AS id FROM #{table(model)}_id_seq"
         {:ok, %{rows: [[last_id]]}} = Ecto.Adapters.SQL.query(__MODULE__, query, [])
         last_id
+      end
+
+      def import_csv(model, path) do
+        file = File.open!(path, [:read])
+        fields = IO.read(file, :line) |> String.slice(0..-2)
+        File.close path
+        table = table model
+        query = ~s(COPY #{table}\(#{fields}\) FROM '#{path}' DELIMITER ',' CSV HEADER)
+        response = Ecto.Adapters.SQL.query(__MODULE__, query, [])
+        query = ~s(SELECT setval\('#{table}_id_seq', COALESCE\(\(SELECT MAX\(id\)+1 FROM #{table}\), 1\), false\))
+        Ecto.Adapters.SQL.query(__MODULE__, query, [])
+        response
+      end
+
+      defp table(model) do
+        model.__schema__(:source)
       end
     end
   end
