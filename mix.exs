31909fa3fb9680545420450bc414f09e6e2fb244
kai.faber <kai.faber@faber.de>
2017-09-06 22:42:43 +0200

    fix bug in setval function of set_table_id_seq

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExUtils.Mixfile do
 
   def project do
     [app: :ex_utils,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
