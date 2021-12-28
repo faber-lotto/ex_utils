c1f123ee3d70bb6d0f5944f7b4fc43b82412103b
Kai Faber <kai.faber@faber.de>
2021-12-28 21:44:12 +0100

    refactor project for compatibility with elixir 1.13

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 # ExUtils
 
-**TODO: Add description**
-
 ## Installation
-
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
 
   1. Add ex_utils to your list of dependencies in `mix.exs`:
 
         def deps do
-          [{:ex_utils, "~> 0.0.1"}]
+          [
+            {:ex_utils, "~> 2.0.0", git: "https://github.com/faber-lotto/ex_utils.git"},
+          ]
         end
 
   2. Ensure ex_utils is started before your application:
--- mix.exs
+++ mix.exs
@@ -1,24 +1,31 @@
-defmodule ExUtils.Mixfile do
+defmodule ExUtils.MixProject do
   use Mix.Project
 
   def project do
-    [app: :ex_utils,
-     version: "1.0.10",
-     elixir: "~> 1.4",
-     elixirc_paths: elixirc_paths(Mix.env),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :ex_utils,
+      version: "2.0.0",
+      elixir: "~> 1.13.0",
+      elixirc_paths: elixirc_paths(Mix.env),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   def application do
-    [applications: [:logger]]
+    [
+      extra_applications: [:logger]
+    ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_),     do: ["lib"]
 
-  defp deps do
-    [{:poison, ">= 3.1.0"}]
+  defp deps() do
+    [
+      {:poison, "~> 5.0.0"},
+      {:ecto_sql, "~> 3.7.0"},
+    ]
   end
 end
